0240f10d286e16f3d725a0b57dfdddb0757a0314
170973 <170973@upslp.edu.mx>
2025-02-26 23:06:08 -0600

    feature_170973

--- scripts.sql
+++ scripts.sql
@@ -7,9 +7,10 @@ CREATE TABLE libros (
 );
 
 INSERT INTO libros (Id, titulo, autor, año_publicacion) VALUES
-(1, 'El psicoanalista','John Katzenbach', 2002),
-(2, 'Aura','Carlos fuente', 1962),
-(3, 'cien años de soledad','Gabriel García Márquez', 1967);
+(1, 'cien años de soledad','Gabriel García Márquez', 1967),
+(2, 'El psicoanalista','John Katzenbach', 2002),
+(3, 'Aura','Carlos fuente', 1962);
+
 
 --
 \echo '\nSección 2: Índices'
@@ -27,25 +28,35 @@ INSERT INTO miembros (Id_m, nombre, fecha_registro) VALUES
 (2, 'Emmanuel Flores', '2025-01-20');
 
 CREATE TABLE prestamos (
-  Id_libro INT REFERENCES libros(Id) NOT NULL,
-  Id_miembro INT REFERENCES miembros(Id_m) NOT NULL
+  Id_libro INT,
+  Id_miembro INT,
+  FOREIGN KEY (Id_libro) REFERENCES libros(Id) ON DELETE CASCADE,
+  FOREIGN KEY (Id_miembro) REFERENCES miembros(Id_m) ON DELETE CASCADE,
+  
+  fecha_prestamo DATE DEFAULT CURRENT_DATE
 );
-INSERT INTO prestamos (Id_libro, Id_miembro) VALUES
-(1, 1),
-(2, 2);
+
+INSERT INTO prestamos  VALUES
+(1, 1, '2025-02-26'),
+(2, 2, '2025-02-20'),
+(3, 2, '2025-02-15');
 
 \echo '\nSección 4: Consultas Avanzadas'
 
-SELECT e.titulo AS nombre_libro, m.nombre AS nombre_miembro
-FROM libros e
-JOIN prestamos em ON em.Id_libro = em.Id_libro
-JOIN miembros m ON em.Id_miembro = em.Id_miembro; 
+SELECT em.titulo AS nombre_libro, m.nombre AS nombre_miembro
+FROM prestamos e
+JOIN libros em ON e.Id_libro = e.Id_libro
+JOIN miembros m ON e.Id_miembro = e.Id_miembro; 
 
 
 SELECT c.nombre AS nombrep, COUNT(ec.Id_miembro) AS catidad_pretamos
 FROM miembros c
 LEFT JOIN prestamos ec ON ec.Id_libro = ec.Id_libro
-GROUP BY c.nombre
+GROUP BY c.nombre;
 
 
-
+SELECT m.nombre AS MIEMBRO
+FROM prestamos p
+JOIN libros l ON p.Id_libro = l.Id
+JOIN miembros m ON p.Id_miembro = l.Id
+WHERE l.titulo = 'cien años de soledad';
