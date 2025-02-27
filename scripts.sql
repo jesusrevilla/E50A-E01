be988e364e8096388dd793c9450a7bf339714555
AntonioMoralesQuiroz <176412@upslp.edu.mx>
2025-02-26 18:54:47 -0600

    176412_scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,44 +1,66 @@
---Crear la tabla libros:
+--Antonio de Jesus Morales Quiroz 176412
+-- create
+CREATE TABLE libros (
+  id INT PRIMARY KEY,
+   título TEXT NOT NULL,
+  autor TEXT NOT NULL,
+  año_publicación  INT
+);
+
+-- insert
+INSERT INTO libros VALUES (0001, 'Cien Años de Soledad', 'Gabriel García Márquez',1990);
+INSERT INTO libros VALUES (0002, 'El Código da Vinci', 'Dan Brown', 2002);
+INSERT INTO libros VALUES (0003, 'El señor de los anillos', 'J. R. R. Tolkien',1980);
+
+-- fetch 
+SELECT * FROM libros;
 
 
---Insertar tres registros en la tabla libros:
+CREATE INDEX inx_título ON libros(título);
+SELECT * FROM libros;
+
+-- create
+CREATE TABLE miembros (
+  id INT PRIMARY KEY,
+   nombre TEXT NOT NULL,
+  fecha_registro DATE
+);
+
+
+INSERT INTO miembros VALUES(1, 'Antonio Morales', '2025-02-25');
+INSERT INTO miembros VALUES(2, 'Araujo','2025-02-26' );
+SELECT * FROM miembros;
+
+
+CREATE TABLE prestamos (
+  id_libro INT ,
+    id_miembro INT,
+     FOREIGN KEY (id_libro) REFERENCES libros(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_miembro) REFERENCES miembros(id) ON DELETE CASCADE
+);
+
+
+INSERT INTO prestamos VALUES(1,2);
+INSERT INTO prestamos VALUES(2,1);
+INSERT INTO prestamos VALUES(3,1);
+
+SELECT * FROM  prestamos;
+
+SELECT l.título AS Titulos,m.nombre AS Miembros
+FROM  libros l
+JOIN prestamos P ON p.id_libro = l.id
+JOIN  miembros m ON p.id_miembro = m.id;
 
 
 
-
---Sección 2: Índices
---Crear un índice en el campo titulo de la tabla libros:
-
-
---Sección 3: Consultas y Relaciones
---Crear la tabla miembros:
+SELECT COUNT(p.id_libro) AS libros, m.nombre AS Miembro
+FROM prestamos p
+JOIN miembros m On m.id = p.id_miembro
+GROUP BY m.nombre;
 
 
---Insertar dos registros en la tabla miembros:
-
-
-
---Crear la tabla prestamos para relacionar libros y miembros:
-
-
-
-
---Insertar registros en la tabla prestamos:
-
-
-
-
---Sección 4: Consultas Avanzadas
---Consulta para obtener los títulos de los libros y los nombres de los miembros que los han prestado:
-
-
-
-
---Consulta para contar cuántos libros ha prestado cada miembro:
-
-
-
-
---Consulta para obtener los nombres de los miembros que han prestado el libro titulado 'Cien Años de Soledad':
-
-
+SELECT m.nombre AS miembros, l.título AS nombres
+FROM miembros m
+JOIN prestamos p ON m.id = p.id_miembro
+JOIN libros l ON p.id_libro = l.id 
+Where l.título = 'Cien Años de Soledad';
