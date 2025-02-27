566d52941be807dc11d5ee97dd8b6debd0428c58
170973 <170973@upslp.edu.mx>
2025-02-26 18:51:38 -0600

    feature/170973.sql

--- scripts.sql
+++ scripts.sql
@@ -1,44 +1,51 @@
---Crear la tabla libros:
+\echo '\nSección 1: Creación y Manipulación de Tablas'
+CREATE TABLE libros (
+  Id INT PRIMARY KEY,
+  titulo VARCHAR(50) NOT NULL,
+  autor VARCHAR(50) NOT NULL,
+  año_publicacion INT NOT NULL
+);
+
+INSERT INTO libros (Id, titulo, autor, año_publicacion) VALUES
+(1, 'El psicoanalista','John Katzenbach', 2002),
+(2, 'Aura','Carlos fuente', 1962),
+(3, 'cien años de soledad','Gabriel García Márquez', 1967);
+
+--
+\echo '\nSección 2: Índices'
+CREATE INDEX nombre_indiceX ON libros (titulo);
+
+\echo '\nSección 3: Consultas y Relaciones'
+CREATE TABLE miembros (
+  Id_m INT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  fecha_registro DATE DEFAULT CURRENT_DATE
+);
+
+INSERT INTO miembros (Id_m, nombre, fecha_registro) VALUES
+(1, 'César Alejandro', '2025-01-15'),
+(2, 'Emmanuel Flores', '2025-01-20');
+
+CREATE TABLE prestamos (
+  Id_libro INT REFERENCES libros(Id) NOT NULL,
+  Id_miembro INT REFERENCES miembros(Id_m) NOT NULL
+);
+INSERT INTO prestamos (Id_libro, Id_miembro) VALUES
+(1, 1),
+(2, 2);
+
+\echo '\nSección 4: Consultas Avanzadas'
+
+SELECT e.titulo AS nombre_libro, m.nombre AS nombre_miembro
+FROM libros e
+JOIN prestamos em ON em.Id_libro = em.Id_libro
+JOIN miembros m ON em.Id_miembro = em.Id_miembro; 
 
 
---Insertar tres registros en la tabla libros:
+SELECT c.nombre AS nombrep, COUNT(ec.Id_miembro) AS catidad_pretamos
+FROM miembros c
+LEFT JOIN prestamos ec ON ec.Id_libro = ec.Id_libro
+GROUP BY c.nombre
 
-
-
-
---Sección 2: Índices
---Crear un índice en el campo titulo de la tabla libros:
-
-
---Sección 3: Consultas y Relaciones
---Crear la tabla miembros:
-
-
---Insertar dos registros en la tabla miembros:
-
-
-
---Crear la tabla prestamos para relacionar libros y miembros:
-
-
-
-
---Insertar registros en la tabla prestamos:
-
-
-
-
---Sección 4: Consultas Avanzadas
---Consulta para obtener los títulos de los libros y los nombres de los miembros que los han prestado:
-
-
-
-
---Consulta para contar cuántos libros ha prestado cada miembro:
-
-
-
-
---Consulta para obtener los nombres de los miembros que han prestado el libro titulado 'Cien Años de Soledad':
 
 
