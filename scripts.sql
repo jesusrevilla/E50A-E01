f87b0e1c9b9aff4845d1da34a76a50cf4fc9a158
aldogg10 <179862@upslp.edu.mx>
2025-09-10 19:42:24 -0600

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -5,9 +5,6 @@ CREATE TABLE libros(
   anio_publicacion INT NOT NULL
 );
 
-INSERT INTO libros VALUES (1, 'Cien años de Soledad', 'Gabriel Garcia Marquez', 1967);
-INSERT INTO libros VALUES (2, 'Born Again', 'Frank Miller', 1980);
-INSERT INTO libros VALUES (3, 'Jurassic Park', 'Ian Malcom', 1973);
 
 CREATE TABLE miembros(
   id_miembros SERIAL PRIMARY KEY,
@@ -15,14 +12,19 @@ CREATE TABLE miembros(
   fecha_registro DATE NOT NULL
 );
 
-INSERT INTO miembros VALUES (1, 'Aldo Garcia', '2025-07-18');
-INSERT INTO miembros VALUES (2, 'Pedro Pascal', '2023-09-10');
 
 CREATE TABLE prestamos(
   id_libro INT REFERENCES libros(id_libro),
   id_miembro INT REFERENCES miembros(id_miembros),
   fecha_prestamo DATE NOT NULL
 );
+
+INSERT INTO libros VALUES (1, 'Cien años de Soledad', 'Gabriel Garcia Marquez', 1967);
+INSERT INTO libros VALUES (2, 'Born Again', 'Frank Miller', 1980);
+INSERT INTO libros VALUES (3, 'Jurassic Park', 'Ian Malcom', 1973);
+
+INSERT INTO miembros VALUES (1, 'Aldo Garcia', '2025-07-18');
+INSERT INTO miembros VALUES (2, 'Pedro Pascal', '2023-09-10');
 
 INSERT INTO prestamos VALUES (3, 1, '2025-09-10');
 INSERT INTO prestamos VALUES (1, 2, '2025-07-9');
